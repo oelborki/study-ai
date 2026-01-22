74861941ba576476c98599961081c27296096ea2
oelborki <156924166+oelborki@users.noreply.github.com>
2026-01-22 02:25:46 -0500

    Disallow allowDangerousEmailAccountLinking

--- src/auth.ts
+++ src/auth.ts
@@ -24,7 +24,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      allowDangerousEmailAccountLinking: true,
     }),
     Credentials({
       name: "credentials",
